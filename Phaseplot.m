13977258ce35ae3f75630b565f80e17fcb135fbe
Thomas Meier <meiers.thomas@bluewin.ch>
2020-05-01 14:52:38 +0200

    added some code to Phaseplot for the temperature debugging

--- Phaseplot.m
+++ Phaseplot.m
@@ -7,7 +7,8 @@ A=textread('axes.in');
 rho = A(3:A(1)+2);
 T = A(A(1)+3:end);
 
-B=textread('phase.txt');
+% B=textread('phase_ice.txt');
+B=textread('phase_iron.txt');
 
 [X,Y]=meshgrid(rho,T);
 
@@ -19,5 +20,10 @@ colorbar
 
 xlabel('Density [g/cm3]')
 ylabel('Temperature [K]')
-title('Phase indicator for DUNITE')
-print2pdf('phaseDUNITE')
+title('Phase indicator for ICE')
+% print2pdf('phaseDUNITE')
+hold on
+% plot3(1.11,273.15,1e10,'x')
+% plot3(1.11,373.15,1e10,'x')
+plot3(7.85,1811,1e10,'x')
+plot3(7.85,3273,1e10,'x')
