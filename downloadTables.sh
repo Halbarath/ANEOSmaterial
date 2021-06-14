d9f1db3b20ec96138697833a919f1bf0d67bffc8
Thomas Meier <meiers.thomas@bluewin.ch>
2021-06-14 10:18:16 +0200

    Added M-ANEOS input files

--- downloadTables.sh
+++ downloadTables.sh
@@ -2,3 +2,6 @@ wget https://www.dropbox.com/s/zu3bpos39yugrgh/ANEOStable_dunite.in
 wget https://www.dropbox.com/s/aofrcfv6hlgo57a/ANEOStable_ice.in
 wget https://www.dropbox.com/s/5sp0zhui6hr71yp/ANEOStable_iron.in
 wget https://www.dropbox.com/s/5kaq65lwalspg24/MANEOStable_water.in
+wget https://www.dropbox.com/s/x6pmfz32bgw3ewm/MANEOStable_dunite.in
+wget https://www.dropbox.com/s/ok62jyds3x2g3j4/MANEOStable_fosterite.in
+wget https://www.dropbox.com/s/gdmcn4hsd886euk/MANEOStable_iron.in
