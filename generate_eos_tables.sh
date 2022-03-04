6314e62667f2bb1342b9ceea649fa32e85cd080f
Christian Reinhardt <christian.reinhardt@ics.uzh.ch>
2022-03-04 21:06:00 +0100

    Check if the ANEOS input files exist. In the future one could add code that automatically generates symbolic links if the input files are not found.

--- generate_eos_tables.sh
+++ generate_eos_tables.sh
@@ -23,7 +23,11 @@ RHO0="7.51"
 OUTPUT="MANEOStable_iron_Fe85Si15.in"
 MAT_STR="M-ANEOS iron alloy Fe85Si15 ($DATE, Stewart 2020: http://doi.org/10.5281/zenodo.3866550)"
 
-"./$MANEOS_EXE" "$INPUT" "$RHO0" "$OUTPUT" "$MAT_STR" 1> /dev/null 2>&1
+if [ -e "$INPUT" ]; then
+    "./$MANEOS_EXE" "$INPUT" "$RHO0" "$OUTPUT" "$MAT_STR" 1> /dev/null 2>&1
+else
+    echo "ANEOS input file $INPUT not found."
+fi
 
 # M-ANEOS iron (Stewart 2020, http://doi.org/10.5281/zenodo.3866507)
 INPUT="aneos-iron-2020.input"
@@ -31,7 +35,11 @@ RHO0="8.06"
 OUTPUT="MANEOStable_iron.in"
 MAT_STR="M-ANEOS iron ($DATE, Stewart 2020: http://doi.org/10.5281/zenodo.3866507)"
 
-"./$MANEOS_EXE" "$INPUT" "$RHO0" "$OUTPUT" "$MAT_STR" 1> /dev/null 2>&1
+if [ -e "$INPUT" ]; then
+    "./$MANEOS_EXE" "$INPUT" "$RHO0" "$OUTPUT" "$MAT_STR" 1> /dev/null 2>&1
+else
+    echo "ANEOS input file $INPUT not found."
+fi
 
 # M-ANEOS forsterite (Stewart 2019, http://doi.org/10.5281/zenodo.3478631)
 INPUT="aneos-forsterite-2019.input"
@@ -40,3 +48,9 @@ OUTPUT="MANEOStable_forsterite.in"
 MAT_STR="M-ANEOS forsterite ($DATE, Stewart 2019: http://doi.org/10.5281/zenodo.3478631)"
 
 "./$MANEOS_EXE" "$INPUT" "$RHO0" "$OUTPUT" "$MAT_STR" 1> /dev/null 2>&1
+
+if [ -e "$INPUT" ]; then
+    "./$MANEOS_EXE" "$INPUT" "$RHO0" "$OUTPUT" "$MAT_STR" 1> /dev/null 2>&1
+else
+    echo "ANEOS input file $INPUT not found."
+fi
