235d1c614fde6a93eee7d55cbd1bd9e0d5b6ce9d
Thomas Meier <meiers.thomas@bluewin.ch>
2023-11-27 09:54:54 +0100

    Updated the table generation and table download scripts

--- downloadTables.sh
+++ downloadTables.sh
@@ -1,7 +1,13 @@
-wget https://www.dropbox.com/s/zu3bpos39yugrgh/ANEOStable_dunite.in
-wget https://www.dropbox.com/s/aofrcfv6hlgo57a/ANEOStable_ice.in
-wget https://www.dropbox.com/s/5sp0zhui6hr71yp/ANEOStable_iron.in
-wget https://www.dropbox.com/s/5kaq65lwalspg24/MANEOStable_water.in
-wget https://www.dropbox.com/s/x6pmfz32bgw3ewm/MANEOStable_dunite.in
-wget https://www.dropbox.com/s/ok62jyds3x2g3j4/MANEOStable_fosterite.in
-wget https://www.dropbox.com/s/gdmcn4hsd886euk/MANEOStable_iron.in
+#/bin/bash
+
+wget -O ANEOStable_dunite.in https://www.dropbox.com/scl/fi/27y4qfecqq25ufkfzdgih/ANEOStable_dunite.in?rlkey=20ek55atddf854ny631vr6ob2&dl=0
+wget -O ANEOStable_ice.in https://www.dropbox.com/scl/fi/negv6bp568fn49z0q9v64/ANEOStable_ice.in?rlkey=u6m5vdpz4bd46gvphh56xnl7l&dl=0
+wget -O ANEOStable_iron.in https://www.dropbox.com/scl/fi/e3yikhb0eg4xlbb9cwduc/ANEOStable_iron.in?rlkey=zv20j3qbg0aqhc6hzzwrc6jbu&dl=0
+wget -O ANEOStable_iron.in https://www.dropbox.com/scl/fi/e3yikhb0eg4xlbb9cwduc/ANEOStable_iron.in?rlkey=zv20j3qbg0aqhc6hzzwrc6jbu&dl=0
+wget -O MANEOStable_dunite.in_ex https://www.dropbox.com/scl/fi/gcput5c3jzca4lmlndqeh/MANEOStable_dunite.in_ex?rlkey=naadt1uj8e7a6jxuebx4jbumw&dl=0
+wget -O MANEOStable_fosterite.in https://www.dropbox.com/scl/fi/6u7b8j81to330849y0u2d/MANEOStable_fosterite.in?rlkey=hsus8kfuhj6lyi3djwgwe3byp&dl=0
+wget -O MANEOStable_fosterite.in_ex https://www.dropbox.com/scl/fi/i0ao6y9erf2yg4dblu18f/MANEOStable_fosterite.in_ex?rlkey=2j0552cjb7w0eph50kxznmsak&dl=0
+wget -O MANEOStable_iron.in https://www.dropbox.com/scl/fi/0p7t60s1z4gv9n5xcn2iz/MANEOStable_iron.in?rlkey=m3u4l5hk2dlibcg0fy99a39on&dl=0
+wget -O MANEOStable_iron.in_ex https://www.dropbox.com/scl/fi/5prbokhuwpasceumv6pad/MANEOStable_iron.in_ex?rlkey=kd9ctqddxrk5dcwo23i7ujmwf&dl=0
+wget -O MANEOStable_iron_Fe85Si15.in https://www.dropbox.com/scl/fi/o0ggiksiih0fteg76vijf/MANEOStable_iron_Fe85Si15.in?rlkey=5yljk3m8g9hoeqrw2g2zyo1uz&dl=0
+wget -O MANEOStable_iron_Fe85Si15.in_ex https://www.dropbox.com/scl/fi/5kq9q24eii8x0kavt3zji/MANEOStable_iron_Fe85Si15.in_ex?rlkey=plev4gtu7ymxj9di3n24y4j4q&dl=0
--- generate_eos_tables.sh
+++ generate_eos_tables.sh
@@ -2,7 +2,7 @@
 # Generate all EOS tables for ANEOSmaterial.
 
 MANEOS_EXE="writeMANEOStable"
-DATE=$(date +%d-%m-%Y)
+DATE=$(date +%d.%m.%Y)
 
 #make clean
 
@@ -18,7 +18,7 @@ if [ ! -e "$MANEOS_EXE" ]; then
 fi
 
 # M-ANEOS iron alloy (Stewart 2020, http://doi.org/10.5281/zenodo.3866550)
-INPUT="aneos-Fe85Si15-2020.input"
+INPUT="../M-ANEOS/input_eoslib/maneos_Fe85Si15_sts2020.input"
 RHO0="7.51"
 OUTPUT="MANEOStable_iron_Fe85Si15.in"
 MAT_STR="M-ANEOS iron alloy Fe85Si15 ($DATE, Stewart 2020: http://doi.org/10.5281/zenodo.3866550)"
@@ -30,7 +30,7 @@ else
 fi
 
 # M-ANEOS iron (Stewart 2020, http://doi.org/10.5281/zenodo.3866507)
-INPUT="aneos-iron-2020.input"
+INPUT="../M-ANEOS/input_eoslib/maneos_iron_sts2020.input"
 RHO0="8.06"
 OUTPUT="MANEOStable_iron.in"
 MAT_STR="M-ANEOS iron ($DATE, Stewart 2020: http://doi.org/10.5281/zenodo.3866507)"
@@ -42,10 +42,24 @@ else
 fi
 
 # M-ANEOS forsterite (Stewart 2019, http://doi.org/10.5281/zenodo.3478631)
-INPUT="aneos-forsterite-2019.input"
+INPUT="../M-ANEOS/input_eoslib/maneos_fosterite_sts2019.input"
 RHO0="3.22"
-OUTPUT="MANEOStable_forsterite.in"
+OUTPUT="MANEOStable_fosterite.in"
 MAT_STR="M-ANEOS forsterite ($DATE, Stewart 2019: http://doi.org/10.5281/zenodo.3478631)"
+
+"./$MANEOS_EXE" "$INPUT" "$RHO0" "$OUTPUT" "$MAT_STR" 1> /dev/null 2>&1
+
+if [ -e "$INPUT" ]; then
+    "./$MANEOS_EXE" "$INPUT" "$RHO0" "$OUTPUT" "$MAT_STR" 1> /dev/null 2>&1
+else
+    echo "ANEOS input file $INPUT not found."
+fi
+
+# M-ANEOS forsterite (Stewart 2019, http://doi.org/10.5281/zenodo.3478631)
+INPUT="../M-ANEOS/input_eoslib/maneos_dunite_gsc.input"
+RHO0="2.65"
+OUTPUT="MANEOStable_dunite.in"
+MAT_STR="M-ANEOS dunite ($DATE, Collins and Melosh 2014)"
 
 "./$MANEOS_EXE" "$INPUT" "$RHO0" "$OUTPUT" "$MAT_STR" 1> /dev/null 2>&1
 
