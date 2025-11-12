9195923b047607dc8c77551bf5a70ad8067ecfce
Christian Reinhardt <christian.reinhardt@ics.uzh.ch>
2025-11-12 11:47:29 +0100

    Add the water eos from Sarah Stewart (2024).

--- ANEOSmaterial.c
+++ ANEOSmaterial.c
@@ -62,6 +62,9 @@ ANEOSMATERIAL *ANEOSinitMaterial(int iMat, double dKpcUnit, double dMsolUnit)
 		case MAT_MANEOS_SERPENTINE:
             strcpy(inputfile, "MANEOStable_serpentine.in");
 			break;
+		case MAT_MANEOS_WATER:
+            strcpy(inputfile, "MANEOStable_water.in");
+			break;
 		default:
 			assert(0);
 	}
