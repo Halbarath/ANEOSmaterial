41287978eec0c5e8198899baeac5e544646de210
Thomas Meier <meiers.thomas@bluewin.ch>
2026-09-18 10:01:46 +0100

    Updated generate_eos_tables.sh to the new repo for the input files, and added serpentine and water

--- generate_eos_tables.sh
+++ generate_eos_tables.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Generate all EOS tables for ANEOSmaterial.
 
-MANEOS_EXE="writeMANEOStable"
+MANEOS_EXE="../ANEOSmaterial/writeMANEOStable"
 DATE=$(date +%d.%m.%Y)
 
 #make clean
@@ -18,7 +18,7 @@ if [ ! -e "$MANEOS_EXE" ]; then
 fi
 
 # M-ANEOS iron alloy (Stewart 2020, http://doi.org/10.5281/zenodo.3866550)
-INPUT="../M-ANEOS/input_eoslib/maneos_Fe85Si15_sts2020.input"
+INPUT="../maneos_input_files/Fe85Si15.input"
 RHO0="7.51"
 OUTPUT="MANEOStable_iron_Fe85Si15.in"
 MAT_STR="M-ANEOS iron alloy Fe85Si15 ($DATE, Stewart 2020: http://doi.org/10.5281/zenodo.3866550)"
@@ -30,7 +30,7 @@ else
 fi
 
 # M-ANEOS iron (Stewart 2020, http://doi.org/10.5281/zenodo.3866507)
-INPUT="../M-ANEOS/input_eoslib/maneos_iron_sts2020.input"
+INPUT="../maneos_input_files/iron.input"
 RHO0="8.06"
 OUTPUT="MANEOStable_iron.in"
 MAT_STR="M-ANEOS iron ($DATE, Stewart 2020: http://doi.org/10.5281/zenodo.3866507)"
@@ -42,7 +42,7 @@ else
 fi
 
 # M-ANEOS forsterite (Stewart 2019, http://doi.org/10.5281/zenodo.3478631)
-INPUT="../M-ANEOS/input_eoslib/maneos_forsterite_sts2019.input"
+INPUT="../maneos_input_files/forsterite.input"
 RHO0="3.22"
 OUTPUT="MANEOStable_forsterite.in"
 MAT_STR="M-ANEOS forsterite ($DATE, Stewart 2019: http://doi.org/10.5281/zenodo.3478631)"
@@ -53,11 +53,39 @@ else
     echo "ANEOS input file $INPUT not found."
 fi
 
-# M-ANEOS forsterite (Stewart 2019, http://doi.org/10.5281/zenodo.3478631)
-INPUT="../M-ANEOS/input_eoslib/maneos_quartz_hjm.input"
+# M-ANEOS quartz (Stewart 2019, http://doi.wiley.com/10.1111/j.1945-5100.2007.tb01009.x)
+INPUT="../maneos_input_files/quartz.input"
 RHO0="2.65"
 OUTPUT="MANEOStable_quartz.in"
-MAT_STR="M-ANEOS dunite ($DATE, Melosh 2007)"
+MAT_STR="M-ANEOS quartz ($DATE, Melosh 2007: http://doi.wiley.com/10.1111/j.1945-5100.2007.tb01009.x)"
+
+"./$MANEOS_EXE" "$INPUT" "$RHO0" "$OUTPUT" "$MAT_STR" 1> /dev/null 2>&1
+
+if [ -e "$INPUT" ]; then
+    "./$MANEOS_EXE" "$INPUT" "$RHO0" "$OUTPUT" "$MAT_STR" 1> /dev/null 2>&1
+else
+    echo "ANEOS input file $INPUT not found."
+fi
+
+# M-ANEOS serpentine (Brookshaw 1998)
+INPUT="../maneos_input_files/serpentine.input"
+RHO0="2.50"
+OUTPUT="MANEOStable_serpentine.in"
+MAT_STR="M-ANEOS serpentine ($DATE, Brookshaw 1998)"
+
+"./$MANEOS_EXE" "$INPUT" "$RHO0" "$OUTPUT" "$MAT_STR" 1> /dev/null 2>&1
+
+if [ -e "$INPUT" ]; then
+    "./$MANEOS_EXE" "$INPUT" "$RHO0" "$OUTPUT" "$MAT_STR" 1> /dev/null 2>&1
+else
+    echo "ANEOS input file $INPUT not found."
+fi
+
+# M-ANEOS water (Stewart 2024, https://zenodo.org/records/14226694)
+INPUT="../maneos_input_files/water.input"
+RHO0="1.25"
+OUTPUT="MANEOStable_water.in"
+MAT_STR="M-ANEOS water ($DATE, Melosh 2007: https://zenodo.org/records/14226694)"
 
 "./$MANEOS_EXE" "$INPUT" "$RHO0" "$OUTPUT" "$MAT_STR" 1> /dev/null 2>&1
 
